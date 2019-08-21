221584e217e0596c772ed2aa738904e92fad808d
Kjetil Hjartnes <kjetil@hjartnes.no>
2019-08-21 12:11:32 +0200

    server identification

--- sgfrename.py
+++ sgfrename.py
@@ -4,7 +4,73 @@ import re
 import sys
 from string import Template
 
-import sgfmill
+default_format = '$date - $server - $black - $white'
+
+server_identifiers = {
+    'OGS': {
+        'tag': 'PC',
+        'value': 'OGS:'
+    },
+    'KGS': {
+        'tag': 'PC',
+        'value': 'The KGS Go Server'
+    },
+    'Tygem': {
+        'tag': 'PC',
+        'value': 'Tygem'
+    },
+    'WBaduk': {
+        'tag': 'PC',
+        'value': 'wbaduk'
+    },
+    'CyberOro': {
+        'tag': 'US',
+        'value': 'www.cyberoro.com'
+    },
+    'IGS': {
+        'tag': 'PC',
+        'value': 'IGS:'
+    },
+    'Fox': {
+        'tag': 'AP',
+        'value': 'foxwq'
+    },
+    'DGS': {
+        'tag': 'PC',
+        'value': 'Dragon Go Server' 
+    },
+    'GoShrine': {
+        'tag': 'PC',
+        'value': 'GoShrine'
+    },
+    'INGO': {
+        'tag': 'PC',
+        'value': 'Played on INGO'
+    },
+    'LankeWeiqi': {
+        'tag': 'PC',
+        'value': '烂柯围棋网'
+    }
+}
+
+def find_key(data, key):
+    reg = r'{0}\[([^]]+)\]'.format(key)
+    matches = re.findall(reg, data)
+    if matches:
+        return matches
+    return None
+
+def find_server(data):
+    for server in server_identifiers:
+        try:
+            info = find_key(data, server_identifiers[server]['tag'])
+            if info:
+                for value in info:
+                    if value.startswith(server_identifiers[server]['value']):
+                        return server
+        except:
+            continue
+    return None
 
 argument_parser = argparse.ArgumentParser()
 argument_parser.add_argument("-r", "--recursive", action='store_true', help="Search folders recursively")
@@ -12,7 +78,17 @@ argument_parser.add_argument("-f", "--format", help="Renaming format")
 
 options = vars(argument_parser.parse_args())
 
-template = Template(options.format)
+#template = Template(options.format)
 
 for file in glob.glob('*.sgf', recursive=options['recursive']):
-    print(file)
+    with open(file, 'r', encoding='utf-8') as f:
+        print(file , ': ')
+        try:
+            data = f.read()
+        except:
+            print('Could not read ' , file)
+            continue
+    black_player = find_key(data, "PB")
+    white_player = find_key(data, "PW")
+
+    
